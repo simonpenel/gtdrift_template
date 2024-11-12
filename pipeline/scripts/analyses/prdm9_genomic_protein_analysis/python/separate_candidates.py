3b384f70a8d33a6b858ee848227b3df7fc985541
Simon Penel <simon.penel@univ-lyon1.fr>
2024-11-12 10:05:46 +0100

    Handling case where there is no blast results

--- pipeline/scripts/analyses/prdm9_genomic_protein_analysis/python/separate_candidates.py
+++ pipeline/scripts/analyses/prdm9_genomic_protein_analysis/python/separate_candidates.py
@@ -10,6 +10,7 @@ args = parser.parse_args()
 with open(args.input, 'r') as reader:
     l = reader.readline()
     first_line = l
+    title = None
     while l:
         if '>' in l:
             if l != first_line:
@@ -23,7 +24,9 @@ with open(args.input, 'r') as reader:
         else:
             seq += l
         l = reader.readline()
-    chr = title.split(' ')[0].split(':')[0][1:]
-    pos = title.split(' ')[0].split(':')[1]
-    with open(args.output + '/' + chr + '@' + pos + '.fna', 'w') as writer:
-        writer.write(title + '\n' + seq)
+
+    if title is not None :
+        chr = title.split(' ')[0].split(':')[0][1:]
+        pos = title.split(' ')[0].split(':')[1]
+        with open(args.output + '/' + chr + '@' + pos + '.fna', 'w') as writer:
+            writer.write(title + '\n' + seq)
