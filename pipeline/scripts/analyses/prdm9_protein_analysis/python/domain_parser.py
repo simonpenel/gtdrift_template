a76ea809406c8110bf7bd28459e9391ab8b610bb
Simon Penel <simon.penel@univ-lyon1.fr>
2024-12-12 15:31:29 +0100

    Use script instead shell

--- pipeline/scripts/analyses/prdm9_protein_analysis/python/domain_parser.py
+++ pipeline/scripts/analyses/prdm9_protein_analysis/python/domain_parser.py
@@ -3,8 +3,11 @@ import os
 print("Input per domain file "+snakemake.input.per_domain)
 print("Output tabulated domain file "+snakemake.output.tabulated_per_domain)
 print("Output summary domain file "+snakemake.output.domain_summary)
+per_domain_file = snakemake.input.per_domain
+tabulated_per_domain_file = snakemake.output.tabulated_per_domain
+summary_per_domain_file = snakemake.output.domain_summary
 
-with open(snakemake.input.per_domain[0]) as reader, open(snakemake.output.per_domain_tabulated[0], 'w') as writer:
+with open(per_domain_file) as reader, open(tabulated_per_domain_file, 'w') as writer:
     for line in reader.readlines():
         if line.startswith('#'):
             del(line)
@@ -13,7 +16,7 @@ with open(snakemake.input.per_domain[0]) as reader, open(snakemake.output.per_do
                 writer.write(f"{elt.strip()}\t")
             writer.write('\n')
 
-with open(snakemake.output.per_domain_tabulated[0]) as reader, open(snakemake.output.domain_summary[0], 'w') as writer:
+with open(tabulated_per_domain_file) as reader, open(summary_per_domain_file, 'w') as writer:
     seq_ID = ''
     newline = ''
     for line in reader.readlines(): 
@@ -26,8 +29,8 @@ with open(snakemake.output.per_domain_tabulated[0]) as reader, open(snakemake.ou
                 newline += f"{elt.strip()}\t"
         else:
             # overlapping zinc finger domains are merged to create one big domain with multiple repetitions.
-            print("\nDEBUG\n\n COMPARE "+snakemake.output.per_domain_tabulated[0]+ " WITH ZF_domains_processed\n\n")
-            if snakemake.output.tabulated[0] == 'ZF_domains_processed':
+            print("\nDEBUG\n\n COMPARE "+tabulated_per_domain_file+ " WITH ZF_domains_processed\n\n")
+            if tabulated_per_domain_file == 'ZF_domains_processed':
                 line_data = line.split(maxsplit=23)
                 newline_data = newline.split('\t')
                 evalue = line_data[12]
