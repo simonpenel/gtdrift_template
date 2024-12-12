39e93e19ab53c8c2127ab6e45f3af56832cdda2c
Simon Penel <simon.penel@univ-lyon1.fr>
2024-12-12 15:20:07 +0100

    Use script instead shell

--- pipeline/scripts/analyses/prdm9_protein_analysis/python/domain_parser.py
+++ pipeline/scripts/analyses/prdm9_protein_analysis/python/domain_parser.py
@@ -1,5 +1,8 @@
 import pandas as pd
 import os
+print("Input per domain file "+snakemake.input.per_domain[0])
+print("Output tabulated domain file "+snakemake.output.per_domain_tabulated[0])
+print("Output summary domain file "+snakemake.output.domain_summary[0])
 
 with open(snakemake.input.per_domain[0]) as reader, open(snakemake.output.per_domain_tabulated[0], 'w') as writer:
     for line in reader.readlines():
