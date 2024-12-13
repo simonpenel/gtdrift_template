f7ca36ca007e32633bab11b05e9ac95f1c2b8c6e
Simon Penel <simon.penel@univ-lyon1.fr>
2024-12-13 13:50:47 +0100

    Correction of getTaxid

--- pipeline/scripts/analyses/prdm9_protein_analysis/module_stats_prdm9.smk
+++ pipeline/scripts/analyses/prdm9_protein_analysis/module_stats_prdm9.smk
@@ -164,19 +164,6 @@ rule formating_hmm_domain_hit:
 #        )
 
 
-# -------------------------------------------------------------
-# function returning the path of all per-domain hits in tabular
-# format with overlapping zinc finger domains.
-# -------------------------------------------------------------
-#def domain_done(wildcards):
-#    return expand(
-#        pathGTDriftData
-#        + "genome_assembly/"
-#        + wildcards.accession
-#        + "/analyses/prdm9_prot/hmm_search/domtbl/{domain}_domains_summary",
-#        domain=DOMAIN,
-#    )
-
 def accession_nb(wildcards):
     return wildcards.accession
 
@@ -193,14 +180,16 @@ rule summarize_hmm_results:
     Creation of a summary table of hmm_search results.
     """
     input:
-        # path of all per-domain hits in tabular format with overlapping zinc finger domains
-        #accession=accession_nb,
+        # organisms_data file
+        organisms_file=pathGTDriftData + "organisms_data",
         # tabulated results of hmm search on SET domain
         SET_per_sequence_tabulated=pathGTDriftData + "genome_assembly/{accession}/analyses/prdm9_prot/hmm_search/tbl/SET_tabulated",
+        # path of all per-domain hits in tabular format
         SET_per_domain_tabulated=pathGTDriftData + "genome_assembly/{accession}/analyses/prdm9_prot/hmm_search/domtbl/SET_domains_tabulated",
         KRAB_per_domain_tabulated=pathGTDriftData + "genome_assembly/{accession}/analyses/prdm9_prot/hmm_search/domtbl/KRAB_domains_tabulated",
         ZF_per_domain_tabulated=pathGTDriftData + "genome_assembly/{accession}/analyses/prdm9_prot/hmm_search/domtbl/ZF_domains_tabulated", 
-        SSXRD_per_domain_tabulated=pathGTDriftData + "genome_assembly/{accession}/analyses/prdm9_prot/hmm_search/domtbl/SSXRD_domains_tabulated",               
+        SSXRD_per_domain_tabulated=pathGTDriftData + "genome_assembly/{accession}/analyses/prdm9_prot/hmm_search/domtbl/SSXRD_domains_tabulated",
+        # path of all per-domain hits in tabular format with overlapping zinc finger domains                     
         SET_per_domain_summary=pathGTDriftData + "genome_assembly/{accession}/analyses/prdm9_prot/hmm_search/domtbl/SET_domains_summary",
         KRAB_per_domain_summary=pathGTDriftData + "genome_assembly/{accession}/analyses/prdm9_prot/hmm_search/domtbl/KRAB_domains_summary",
         ZF_per_domain_summary=pathGTDriftData + "genome_assembly/{accession}/analyses/prdm9_prot/hmm_search/domtbl/ZF_domains_summary", 
@@ -216,14 +205,7 @@ rule summarize_hmm_results:
         accession=accession_nb,  
     script:
         "python/table_builder.py"
-#    shell:
-#        (
-#            "python3 "
-#            + pathGTDriftScripts
-#            + "analyses/prdm9_protein_analysis/python/table_builder.py -i "
-#            + pathGTDriftData
-#            + "genome_assembly -a {wildcards.accession} -o {output}"
-#        )
+
 
 
 # -------------------------------------------------------------------
--- pipeline/scripts/analyses/prdm9_protein_analysis/python/table_builder.py
+++ pipeline/scripts/analyses/prdm9_protein_analysis/python/table_builder.py
@@ -1,16 +1,8 @@
 import pandas as pd
 
-
-#parser = argparse.ArgumentParser(description='Reads hmm_search processed files and domains processed files and creates an overview table in the csv format')
-
-#parser.add_argument('-i', '--input_dir', type=str, required=True, help='Input dir path')
-#parser.add_argument('-a', '--accession', type=str, required=True, help='Organism genome NCBI accession number')
-#parser.add_argument('-o', '--output', type=str, required=True, help='Output file path')
-#parser.add_argument('-s', '--set', type=str, required=True, help='Tabulated SET domain hmmsearch results file path')
-#args = parser.parse_args()
-
 accession_number = snakemake.params.accession
-#input_dir = args.input_dir
+
+organisms_file = snakemake.input.organisms_file
 
 SET_per_sequence_tabulated_file = snakemake.input.SET_per_sequence_tabulated
 
@@ -19,26 +11,21 @@ KRAB_per_domain_tabulated_file = snakemake.input.KRAB_per_domain_tabulated
 SSXRD_per_domain_tabulated_file = snakemake.input.SSXRD_per_domain_tabulated
 ZF_per_domain_tabulated_file = snakemake.input.ZF_per_domain_tabulated
 
-
 SET_per_domain_summary_file = snakemake.input.SET_per_domain_summary
 KRAB_per_domain_summary_file = snakemake.input.KRAB_per_domain_summary
 SSXRD_per_domain_summary_file = snakemake.input.SSXRD_per_domain_summary
 ZF_per_domain_summary_file = snakemake.input.ZF_per_domain_summary
 
-
 output_file = snakemake.output[0]
 
-#SET_hmm_tab = args.s
 noms_colonnes = ['SeqID', 'SET Query', 'SET E-value', 'SET Score', 'Nb SET domains', 'SET domain start', 'SET domain end',
                 'KRAB Query', 'KRAB E-value', 'KRAB Score', 'Nb KRAB domains', 'KRAB domain start', 'KRAB domain end',
                 'SSXRD Query', 'SSXRD E-value', 'SSXRD Score', 'Nb SSXRD domains', 'SSXRD domain start', 'SSXRD domain end',
                 'ZF Query', 'ZF E-value', 'ZF Score', 'Nb ZF domains', 'ZF domain start', 'ZF domain end']
 
-
 data_list = []
 
 # All candidates must have a SET domain
-#with open(f"{input_dir}/{accession_number}/analyses/prdm9_prot/hmm_search/tbl/SET_tabulated") as reader:
 with open(SET_per_sequence_tabulated_file) as reader:
     for line in reader:
         line_data = line.strip().split('\t')
@@ -52,7 +39,6 @@ def process_domain_tabulated(domain, domain_tabulated_file, accession_number=acc
     '''
     Lit les fichiers résultat de hmm_search après mise en forme (1 fichier pour chaque domain protéique) et saisit les valeurs d'intérêt (E-value, Score) dans un data frame
     '''
-#    with open(f"{input_dir}/{accession_number}/analyses/prdm9_prot/hmm_search/domtbl/{domain}_domains_tabulated") as reader:
     with open(domain_tabulated_file) as reader:    
         for line in reader.readlines():
             line_data = line.split('\t')
@@ -62,12 +48,10 @@ def process_domain_tabulated(domain, domain_tabulated_file, accession_number=acc
                 summarised_data.loc[summarised_data['SeqID'] == seq_id, f"{domain} E-value"] = line_data[6]
                 summarised_data.loc[summarised_data['SeqID'] == seq_id, f"{domain} Score"] = line_data[7]
 
-
 def process_domain_summary(domain, domain_summary_file, accession_number=accession_number):
     '''
     Récupère les informations importantes (nombre de domaines identifiés, position) dans les fichiers résultat de hmm_search --domtblout et les saisit dans un dataframe
     '''
-#    with open(f"{input_dir}/{accession_number}/analyses/prdm9_prot/hmm_search/domtbl/{domain}_domains_summary") as reader:
     with open(domain_summary_file) as reader:    
         for line in reader.readlines()[1:]:
             line_data = line.split('\t')
@@ -78,14 +62,12 @@ def process_domain_summary(domain, domain_summary_file, accession_number=accessi
                 summarised_data.loc[summarised_data['SeqID'] == seq_id, f"{domain} domain start"] = line_data[17]
                 summarised_data.loc[summarised_data['SeqID'] == seq_id, f"{domain} domain end"]= line_data[18]
 
-#def getTaxid(accession_number=accession_number,input_dir=input_dir):
-#    df = pd.read_csv(input_dir+'/../organisms_data', sep='\t', header=0)
-#    taxid = df.loc[df['Assembly Accession'] == accession_number, 'Taxid'].values[0]    
-#    print("DEBUG "+accession_number+": "+str(taxid))
-#    summarised_data["Taxid"] = taxid
+def getTaxid(accession_number=accession_number,input_file=organisms_file):
+    df = pd.read_csv(organisms_file, sep='\t', header=0)
+    taxid = df.loc[df['Assembly Accession'] == accession_number, 'Taxid'].values[0]    
+    print("DEBUG "+accession_number+": "+str(taxid))
+    summarised_data["Taxid"] = taxid
 
-
-#domain1 = 'KRAB'
 process_domain_tabulated("KRAB",KRAB_per_domain_tabulated_file )
 process_domain_summary("KRAB",KRAB_per_domain_summary_file )
 
@@ -98,16 +80,7 @@ process_domain_summary("SSXRD",SSXRD_per_domain_summary_file )
 process_domain_tabulated("ZF",ZF_per_domain_tabulated_file )
 process_domain_summary("ZF",ZF_per_domain_summary_file )
 
-#processed_domains(domain1)
-#domain2 = 'SSXRD'
-#processed_data(domain2)
-#processed_domains(domain2)
-#domain3 = 'ZF'
-#processed_data(domain3)
-#processed_domains(domain3)
-#domain4 = 'SET'
-#processed_domains(domain4)
-# a remettre getTaxid()
+getTaxid()
 
 summarised_data = summarised_data.fillna(0)                                   
 summarised_data.to_csv(output_file, sep=';')
