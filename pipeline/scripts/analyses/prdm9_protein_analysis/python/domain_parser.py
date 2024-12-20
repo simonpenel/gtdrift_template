69d48e1590042b867ac5aeed3334c758a9338346
Simon Penel <simon.penel@univ-lyon1.fr>
2024-12-20 10:01:39 +0100

    Start to remove useless code

--- pipeline/scripts/analyses/prdm9_protein_analysis/python/domain_parser.py
+++ pipeline/scripts/analyses/prdm9_protein_analysis/python/domain_parser.py
@@ -16,40 +16,42 @@ with open(per_domain_file) as reader, open(tabulated_per_domain_file, 'w') as wr
                 writer.write(f"{elt.strip()}\t")
             writer.write('\n')
 
-with open(tabulated_per_domain_file) as reader, open(summary_per_domain_file, 'w') as writer:
+#with open(tabulated_per_domain_file) as reader, open(summary_per_domain_file, 'w') as writer:
+with open(per_domain_file) as reader, open(summary_per_domain_file, 'w') as writer:
     seq_ID = ''
     newline = ''
-    for line in reader.readlines(): 
-        current_seq_ID = line.split('\t')[0]
-        if current_seq_ID != seq_ID:
-            seq_ID = current_seq_ID
-            writer.write(newline + '\n')
-            newline = ''
-            for elt in line.split(maxsplit=23):
-                newline += f"{elt.strip()}\t"
-        else:
-            # overlapping zinc finger domains are merged to create one big domain with multiple repetitions.
-            test_if_zf = tabulated_per_domain_file.split("/")[-1]
-            #print("Check if "+test_if_zf+ " is ZF_domains_tabulated :")
-            if test_if_zf == 'ZF_domains_tabulated':
-                #print("Yes.")
-                line_data = line.split(maxsplit=23)
-                newline_data = newline.split('\t')
-                evalue = line_data[12]
-                start = line_data[17]
-                end = line_data[18]
-                newline_data[12] = str(min(float(evalue), float(newline_data[12])))
-                newline_data[17] = str(min(int(start), int(newline_data[17])))
-                newline_data[18] = str(max(int(end), int(newline_data[18])))
-                newline = '\t'.join(newline_data)
-            else:
-                #print("No.")
+    for line in reader.readlines():
+        if not line.startswith('#'): 
+            current_seq_ID = line.split('\t')[0]
+            if current_seq_ID != seq_ID:
+                seq_ID = current_seq_ID
                 writer.write(newline + '\n')
                 newline = ''
                 for elt in line.split(maxsplit=23):
                     newline += f"{elt.strip()}\t"
+            else:
+                # overlapping zinc finger domains are merged to create one big domain with multiple repetitions.
+                test_if_zf = tabulated_per_domain_file.split("/")[-1]
+                #print("Check if "+test_if_zf+ " is ZF_domains_tabulated :")
+                if test_if_zf == 'ZF_domains_tabulated':
+                    #print("Yes.")
+                    line_data = line.split(maxsplit=23)
+                    newline_data = newline.split('\t')
+                    evalue = line_data[12]
+                    start = line_data[17]
+                    end = line_data[18]
+                    newline_data[12] = str(min(float(evalue), float(newline_data[12])))
+                    newline_data[17] = str(min(int(start), int(newline_data[17])))
+                    newline_data[18] = str(max(int(end), int(newline_data[18])))
+                    newline = '\t'.join(newline_data)
+                else:
+                    #print("No.")
+                    writer.write(newline + '\n')
+                    newline = ''
+                    for elt in line.split(maxsplit=23):
+                        newline += f"{elt.strip()}\t"
                 
-    writer.write(newline + '\n')
+            writer.write(newline + '\n')
         
 
 
