768d648b8f4bac4c5280b36f1aa68b834f8d636b
Simon Penel <simon.penel@univ-lyon1.fr>
2025-07-21 16:22:23 +0200

    A same fatsa file for the 2 patterns

--- pipeline/scripts/analyses/utils/python/extract_zf_fasta.py
+++ pipeline/scripts/analyses/utils/python/extract_zf_fasta.py
@@ -32,38 +32,33 @@ fl = open(args.output_dir+"/list_of_files.txt", "w")
 for seqid in seqids:
     print(seqid)
     extract_seqid = df[df["SeqID"] == seqid]
-    patterns = np.unique(extract_seqid["Pattern num"])
-    print(patterns)
-    for pattern in patterns :
-        extract_pattern = extract_seqid[extract_seqid["Pattern num"] == pattern]
-        #print(str(pattern))
-        #print(extract_pattern)
-        contigs = np.unique(extract_pattern["Contig"])
-        for contig in contigs :
-            extract_contig = extract_pattern[extract_pattern["Contig"] == contig]
-            #print(contig)
-            #print(extract_contig)
-            sequences = extract_contig["dna sequence reading strand"]
-            print(sequences)
-            zf_names = []
-            for name in extract_contig["ZF name"]:
-                zf_names.append(name)
-
+    #patterns = np.unique(extract_seqid["Pattern num"])
+    #print(patterns)
+    contigs = np.unique(extract_seqid["Contig"])
+    for contig in contigs :
+        extract_contig = extract_seqid[extract_seqid["Contig"] == contig]
+        #print(contig)
+        #print(extract_contig)
+        sequences = extract_contig["dna sequence reading strand"]
+        print(sequences)
+        zf_names = []
+        for name in extract_contig["ZF name"]:
+            zf_names.append(name)
             #zf_names = extract_contig["ZF name"]
-            print(zf_names)
-            print("SEQUENCE "+seqid+" PATTERN "+str(pattern)+" CONTIG "+ contig+":\n" )
-            #print(sequences)
-            file_name = seqid+"_p"+str(pattern)+"_"+contig
-            description = "Sequence :"+seqid+"; Pattern :"+str(pattern)+"; Contig:"+contig
-            my_records = []
-            zf_number = 1
-            for sequence in sequences:
-                dna_seq = Seq(sequence)
+        print(zf_names)
+        print("SEQUENCE "+seqid+" CONTIG "+ contig+":\n" )
+        #print(sequences)
+        file_name = seqid+"_"+contig
+        description = "Sequence :"+seqid+"; Contig:"+contig
+        my_records = []
+        zf_number = 1
+        for sequence in sequences:
+            dna_seq = Seq(sequence)
 
-                record = SeqRecord(dna_seq, id=zf_names[zf_number -1],description="Zinc finger ;"+description,)
+            record = SeqRecord(dna_seq, id=zf_names[zf_number -1],description="Zinc finger ;"+description,)
                 #print(record)
-                my_records.append(record)
-                zf_number +=1
-            count = SeqIO.write(my_records, args.output_dir+"/"+file_name+".fasta", "fasta")
-            fl.write(file_name+".fasta\n")
+            my_records.append(record)
+            zf_number +=1
+        count = SeqIO.write(my_records, args.output_dir+"/"+file_name+".fasta", "fasta")
+        fl.write(file_name+".fasta\n")
 fl.close()
