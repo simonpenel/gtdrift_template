51a56216a1c85928d5a1baa8a7782e60c2b49a1a
Simon Penel <simon.penel@univ-lyon1.fr>
2025-07-31 14:28:33 +0200

    New csv output format

--- pipeline/scripts/analyses/utils/python/extract_zf_fasta.py
+++ pipeline/scripts/analyses/utils/python/extract_zf_fasta.py
@@ -34,7 +34,13 @@ def zfsum(zf:[], positions:[]):
         sum = sum / total
     return sum
  
-
+def zfmean(zf:[], positions:[]):
+    sum = 0
+    for pos in positions:
+        sum = sum + zf[pos]
+    mean = sum / len(position)
+    return sum
+    
 # Calculate the zfd
 def zfd(seqs:[]):
     zfd = []
@@ -119,19 +125,7 @@ def write_divindex(zfd_data, zfd_codon_data, zfd_name):
         fclustsummary.write('{:>5}'.format(leg))
         ipos += 1       
     fclustsummary.write("\n") 
-    fclustsummary.write("#==================== CSV =======================\n")  
-    fclustsummary.write(zfd_name + " div. index aa;")
-    for div in zfd_data:
-        fclustsummary.write('%5.2f' % (div))
-        fclustsummary.write(";")
-    fclustsummary.write("\n")
-    
-    fclustsummary.write(zfd_name + " div. index  codons;")
-    for div in zfd_codon_data:
-        fclustsummary.write('%5.2f' % (div))
-        fclustsummary.write(";")
-    fclustsummary.write("\n")   
-    fclustsummary.write("#==================== CSV =======================\n")  
+
     
 def write_divindex_pos(zfd_data,zfd_name):
             zfd_pos = zfsum(zfd_data,positions_contact )
@@ -140,6 +134,7 @@ def write_divindex_pos(zfd_data,zfd_name):
             fclustsummary.write('{:45}'.format("# " + zfd_name + " ZFD " + pos_string+ " : "))
             fclustsummary.write('%6.3f' % (zfd_pos))
             fclustsummary.write("\n")
+            df_csv.loc[df_csv['ZF_Dataset'] == zfd_name, "ZFD"] = round(zfd_pos, 2)
 
 def calcul_synonym_divindex(zfd_data, zfd_codon_data, zfd_name):    
     zdf_syno = []
@@ -147,6 +142,7 @@ def calcul_synonym_divindex(zfd_data, zfd_codon_data, zfd_name):
     total_syno = 0.0
     total_ratio = 0.0
     total_ratio_no_pos = 0.0 # on exclut les position d'interet
+    total_ratio_pos = 0.0 # on garde les position d'interet
     for i in range(0,len(zfd_data)):
         syno = zfd_codon_data[i] - zfd_data[i]
         if syno < 0:
@@ -172,9 +168,12 @@ def calcul_synonym_divindex(zfd_data, zfd_codon_data, zfd_name):
             total_ratio += ratio
             if i not in positions_contact :
                 total_ratio_no_pos += ratio
+            else :
+                total_ratio_pos += ratio
 
     mean_ratio = total_ratio / (len(zfd_data)   - len(positions_to_exclude))    
     mean_ratio_no_pos = total_ratio_no_pos / (len(zfd_data)   - len(positions_to_exclude) -len(positions_contact) )    
+    mean_ratio_pos = total_ratio_pos / len(positions_contact)
     fclustsummary.write('{:45}'.format("# " + zfd_name + " syno (codon - aa) : "))
     for div in zdf_syno:
         fclustsummary.write('%5.2f' % (div))
@@ -190,6 +189,10 @@ def calcul_synonym_divindex(zfd_data, zfd_codon_data, zfd_name):
     fclustsummary.write('%5.2f' % (mean_ratio))       
     fclustsummary.write("\n")
     
+    
+    df_csv.loc[df_csv['ZF_Dataset'] == zfd_name, "Mean_divS"] = round(mean_syno, 2)
+    df_csv.loc[df_csv['ZF_Dataset'] == zfd_name, "Mean_ratio_AS"] = round(mean_ratio, 2)
+    jpos = 3
     for pos in positions_contact:
         fclustsummary.write('{:45}'.format("# " + zfd_name + " position "+str(pos+1)))
         fclustsummary.write(" Ratio : ")     
@@ -197,6 +200,11 @@ def calcul_synonym_divindex(zfd_data, zfd_codon_data, zfd_name):
         fclustsummary.write(" Mean ratio excluding contact positions :")    
         fclustsummary.write('%5.2f' % (mean_ratio_no_pos)) 
         fclustsummary.write("\n")
+        as_name = "Ratio_AS"+legend[jpos]
+        df_csv.loc[df_csv['ZF_Dataset'] == zfd_name, as_name] = round(zdf_ratio[pos], 2)
+        jpos = jpos -1
+    df_csv.loc[df_csv['ZF_Dataset'] == zfd_name, "Mean_ratio_AS_non1236"] = round(mean_ratio_no_pos, 2)
+    df_csv.loc[df_csv['ZF_Dataset'] == zfd_name, "Mean_ratio_AS1236"] = round(mean_ratio_pos, 2)    
     #fclustsummary.write("\n")
     
        
@@ -215,6 +223,18 @@ for seqid in seqids:
     contigs = np.unique(extract_seqid["Contig"])
     for contig in contigs :
     
+        # Creation du df pour le csv
+
+        df_csv = pd.DataFrame(columns=["ZF_Dataset","ID_set", "Nb_ZF","Nb_ZF_28","Nb_ZF_29","Nb_Arrays","Nb_Clusters","Nb_singletons","Mean_divS", "Mean_ratio_AS","Ratio_AS-1","Ratio_AS2","Ratio_AS3","Ratio_AS6","Mean_ratio_AS1236" ,"Mean_ratio_AS_non1236" ,"ZFD"])
+        new_row = pd.DataFrame({"ZF_Dataset" : "All_ZFs" }, index=[0])
+        df_csv = pd.concat([df_csv.loc[:],new_row]).reset_index(drop=True)
+
+        new_row = pd.DataFrame({"ZF_Dataset" : "Longest_ZF_array" }, index=[0])
+        df_csv = pd.concat([df_csv.loc[:],new_row]).reset_index(drop=True)
+        
+        new_row = pd.DataFrame({"ZF_Dataset" : "Largest_ZF_cluster" }, index=[0])
+        df_csv = pd.concat([df_csv.loc[:],new_row]).reset_index(drop=True)
+                
         # prefixe des noms de fichiers
         file_name = seqid+"_"+contig
         
@@ -288,15 +308,27 @@ for seqid in seqids:
             if len(list_array_total[array]) > size_array_max:
                 array_max = array
                 size_array_max = len(list_array_total[array])
-                
+                        
+        df_csv.loc[df_csv['ZF_Dataset'] == "All_ZFs", "Nb_ZF_28"] = nb_28
+        df_csv.loc[df_csv['ZF_Dataset'] == "All_ZFs", "Nb_ZF_29"] = nb_29
+        
         # Calcul de la diversite sur le plus grand array
         proteins_array_max =  []
         dna_array_max =  []
+        nb_28 = 0
+        nb_29 = 0
         for name in  list_array_total[array_max]:
             protein_seq = dico_protein[name].seq
             proteins_array_max.append(protein_seq)
             dna_seq = dico_sequence[name].seq
             dna_array_max.append(dna_seq)
+            suff = name[-2:]
+            if suff == "28":
+                nb_28 +=1
+            elif suff == "29":
+                nb_29 +=1  
+        df_csv.loc[df_csv['ZF_Dataset'] == "Longest_ZF_array", "Nb_ZF_28"] = nb_28
+        df_csv.loc[df_csv['ZF_Dataset'] == "Longest_ZF_array", "Nb_ZF_29"] = nb_29                
         zfd_array = zfd(proteins_array_max)    
         zfd_codon_array = zfd_codon(dna_array_max)  
                   
@@ -321,16 +353,7 @@ for seqid in seqids:
                     families[fam].append(seq)
             clusters = list(families.keys())
 
-            fclustsummary=open(args.output_dir+"/"+file_name+".clust_summary", "w") 
-            fclustsummary.write("#==================== CSV =======================\n") 
-            fclustsummary.write("Diversity index;")
-            ipos = 1 
-            for div in zfd_array:
-                fclustsummary.write('%5d' % (ipos))
-                fclustsummary.write(";")
-                ipos += 1       
-            fclustsummary.write("\n") 
-            fclustsummary.write("#==================== CSV =======================\n")    
+            fclustsummary=open(args.output_dir+"/"+file_name+".clust_summary", "w")    
             fclustsummary.write("# Nb of zf : "+str(len(sequences))+"\n")   
             fclustsummary.write("#\n")
             fclustsummary.write("#################################################\n")
@@ -339,13 +362,13 @@ for seqid in seqids:
             
             if zf_number > 4:
             
-                write_divindex(zfd_all, zfd_codon_all, "Global")
+                write_divindex(zfd_all, zfd_codon_all, "All_ZFs")
 
-                calcul_synonym_divindex(zfd_all, zfd_codon_all, "Global")
+                calcul_synonym_divindex(zfd_all, zfd_codon_all, "All_ZFs")
             
-                write_divindex_pos(zfd_all, "Global")    
+                write_divindex_pos(zfd_all, "All_ZFs")    
              
-                write_divindex_pos(zfd_codon_all, "Global codons")       
+                write_divindex_pos(zfd_codon_all, "All_ZFs codons")       
       
             else :
             
@@ -357,23 +380,29 @@ for seqid in seqids:
             fclustsummary.write("# Longest array : " + array_max + "\n")
             fclustsummary.write("# Size longest array : " + str(size_array_max)+ "\n") 
             
+            df_csv.loc[df_csv['ZF_Dataset'] == "All_ZFs", "Nb_ZF"] = zf_number
+            df_csv.loc[df_csv['ZF_Dataset'] == "Longest_ZF_array", "ID_set"] = array_max
+            df_csv.loc[df_csv['ZF_Dataset'] == "All_ZFs", "Nb_Arrays"] = len(list(list_array_total.keys()))
+            
             fclustsummary.write("#\n")
             fclustsummary.write("#################################################\n")
             fclustsummary.write("# Longest ZF array\n")
             fclustsummary.write("#################################################\n")
             
+            df_csv.loc[df_csv['ZF_Dataset'] == "Longest_ZF_array", "Nb_ZF"] = size_array_max
             if size_array_max > 4 :
-                write_divindex(zfd_array, zfd_codon_array, "Longest array")               
+                write_divindex(zfd_array, zfd_codon_array, "Longest_ZF_array")               
             
-                calcul_synonym_divindex(zfd_array, zfd_codon_array, "Longest array")            
+                calcul_synonym_divindex(zfd_array, zfd_codon_array, "Longest_ZF_array")            
             
-                write_divindex_pos(zfd_array, "Longest array")        
+                write_divindex_pos(zfd_array, "Longest_ZF_array")        
 
-                write_divindex_pos(zfd_codon_array, "Longest array codons")  
+                write_divindex_pos(zfd_codon_array, "Longest_ZF_array codons")  
             
             else :
                 fclustsummary.write("# Less than 5 zf:  no stats.\n")              
             
+            df_csv.loc[df_csv['ZF_Dataset'] == "All_ZFs", "Nb_Clusters"] = len(clusters)
             if len(clusters) == 0: 
                 fclustsummary.write("# Nb of clusters : 0\n")             
                 fclustsummary.close()
@@ -407,16 +436,33 @@ for seqid in seqids:
                 fclustsummary.write("# Nb of singletons : "+ str(len(orphans))+"\n")
                 seqs = families[cluster_max]
                 fclustsummary.write("# Cluster max contents: ")
+                
+                df_csv.loc[df_csv['ZF_Dataset'] == "Largest_ZF_cluster", "ID_set"] = "C_"+cluster_max
+                df_csv.loc[df_csv['ZF_Dataset'] == "Largest_ZF_cluster", "Nb_ZF"] = nb_seq_max
+                df_csv.loc[df_csv['ZF_Dataset'] == "All_ZFs", "Nb_singletons"] = len(orphans)
+                
+                
+                
                 list_array = []
                 cluster_max_protein_sequence = []
                 cluster_max_dna_sequence = []
+                nb_28 = 0
+                nb_29 = 0
                 for seq in seqs:
+                    suff = seq[-2:]
+                    if suff == "28":
+                        nb_28 +=1
+                    elif suff == "29":
+                        nb_29 +=1  
                     cluster_max_protein_sequence.append(dico_protein[seq].seq)
                     cluster_max_dna_sequence.append(dico_sequence_bck[seq].seq)
                     fclustsummary.write(seq+"_C"+cluster_max+" ")
                     arr = seq[0]
                     if not (arr in list_array) :
                         list_array.append(arr)
+                        
+                df_csv.loc[df_csv['ZF_Dataset'] == "Largest_ZF_cluster", "Nb_ZF_28"] = nb_28
+                df_csv.loc[df_csv['ZF_Dataset'] == "Largest_ZF_cluster", "Nb_ZF_29"] = nb_29                          
                 fclustsummary.write("\n")
                 zfd_cluster = zfd(cluster_max_protein_sequence) 
                 zfd_codon_cluster = zfd_codon(cluster_max_dna_sequence)  
@@ -427,18 +473,20 @@ for seqid in seqids:
                 fclustsummary.write("#################################################\n")
                 
                 if nb_seq_max > 4 :              
-                    write_divindex(zfd_cluster, zfd_codon_cluster, "Longest cluster")
+                    write_divindex(zfd_cluster, zfd_codon_cluster, "Largest_ZF_cluster")
 
-                    calcul_synonym_divindex(zfd_cluster, zfd_codon_cluster, "Longest cluster") 
+                    calcul_synonym_divindex(zfd_cluster, zfd_codon_cluster, "Largest_ZF_cluster") 
                             
-                    write_divindex_pos(zfd_cluster, "Longest cluster")
+                    write_divindex_pos(zfd_cluster, "Largest_ZF_cluster")
                 
-                    write_divindex_pos(zfd_codon_cluster, "Longest cluster codons")                    
+                    write_divindex_pos(zfd_codon_cluster, "Largest_ZF_cluster codons")                    
                 
                 else :
                     fclustsummary.write("# Less than 5 zf:  no stats.\n")    
                 
                 fclustsummary.write("# Nb of arrays in cluster max: "+str(len(list_array))+"\n") 
+                df_csv.to_csv(fclustsummary,sep=';' , index=False , na_rep="NA")
                 fclustsummary.close() 
+                print(df_csv)
                 count = SeqIO.write(new_records, args.output_dir+"/"+file_name+"_cluster.fasta", "fasta")    
 fl.close()
