cc4507314469ac00b2a54f0dfc41cf3c35318ad5
Simon Penel <simon.penel@univ-lyon1.fr>
2025-09-14 19:37:50 +0200

    Ok with  2025-09-14T182219.551490.snakemake.log

--- pipeline/scripts/analyses/utils/python/get_ZF_motif_positions_in_protein.py
+++ pipeline/scripts/analyses/utils/python/get_ZF_motif_positions_in_protein.py
@@ -361,7 +361,9 @@ for seq_record in SeqIO.parse(args.input, "fasta"):
             if cds_strand  == "+" :
                 start_prot = start_prot + frame_first_cds
                 #end_prot = end_prot - frame_last_cds
-                end_prot = end_prot - 3 + frame_last_cds
+                #end_prot = end_prot - 3 + frame_last_cds
+                # ok 2025-09-14T182219.551490.snakemake.log
+                end_prot = end_prot + 3 - frame_last_cds
             else :
                 start_prot = start_prot + frame_first_cds
                 end_prot = end_prot - frame_last_cds
