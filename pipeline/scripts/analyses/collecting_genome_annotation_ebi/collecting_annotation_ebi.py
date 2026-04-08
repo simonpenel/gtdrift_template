d0b03931627a868957de680dd45c7b938b99f09e
Simon Penel <simon.penel@univ-lyon1.fr>
2026-04-08 15:50:36 +0200

    Atempt to create a second ouput file dedicated to reference strains. Not working yet

--- pipeline/scripts/analyses/collecting_genome_annotation_ebi/collecting_annotation_ebi.py
+++ pipeline/scripts/analyses/collecting_genome_annotation_ebi/collecting_annotation_ebi.py
@@ -16,6 +16,7 @@ server = "https://rest.ensembl.org"
 ext = "/info/genomes/taxonomy/Primates?"
 ext = "/info/genomes/taxonomy/Chordata?"
 ext = "/info/genomes/taxonomy/Metazoa?"
+#ext = "/info/genomes/taxonomy/Ovis?"
 r = requests.get(server+ext, headers={ "Content-Type" : "application/json"})
  
 if not r.ok:
@@ -29,6 +30,7 @@ accessions = []
 for line in decoded:
     scientific_name = line["scientific_name"]
     assembly_name = line["assembly_name"]
+    strain = line["strain"]
     assembly_accession = line["assembly_accession"]
     taxid = line["species_taxonomy_id"]
     division = line["division"]
@@ -50,14 +52,19 @@ for line in decoded:
     print(url_fasta_pep)    
     url_gff3 = url_index + "gff3/" + line["name"] + "/" + line["url_name"] + "." +  line["assembly_default"] + "." + version + ".gff3.gz"
     print(url_gff3)
-    dico[index] = [scientific_name,taxid,assembly_name,assembly_accession,division,url_fasta_cds,url_fasta_pep,url_fasta_genome,url_gff3] 
+    dico[index] = [scientific_name,strain,taxid,assembly_name,assembly_accession,division,url_fasta_cds,url_fasta_pep,url_fasta_genome,url_gff3] 
     accessions.append(assembly_accession)
     index +=1
 
 sum = pd.DataFrame(dico).T
-sum.columns = ['Species Name','Taxid','Assembly Name','Assembly Accession',"Division","URL fasta cds","URL fasta proteins","URL fasta genome","URL gff3 genome"]
+sum.columns = ['Species Name','Strain','Taxid','Assembly Name','Assembly Accession',"Division","URL fasta cds","URL fasta proteins","URL fasta genome","URL gff3 genome"]
 print(sum)
 sum.to_csv("organisms_data_ebi",sep="\t", index=False )
+ref = sum[(sum['Strain'] == 'reference')]
+ref2 = sum[(sum['Strain'].isnull())]
+ref = pd.concat([ref,ref2]).reset_index(drop=True)
+ref.to_csv("organisms_data_ebi.ref_only",sep="\t", index=False )
+
 
 data = {
     "assembly_list" : accessions,
