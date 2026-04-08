a00dbeb84cae1380f2355840395830c4bed71f50
Simon Penel <simon.penel@univ-lyon1.fr>
2026-04-08 11:25:31 +0200

    Add metazoa division

--- pipeline/scripts/analyses/collecting_genome_annotation_ebi/collecting_annotation_ebi.py
+++ pipeline/scripts/analyses/collecting_genome_annotation_ebi/collecting_annotation_ebi.py
@@ -5,13 +5,17 @@ import requests, sys
 import numpy as np
 import pandas as pd
 import json
-
 # URL to download
-url_index = "https://ftp.ensembl.org/pub/" 
+release_vertebrata = "115"
+release_metazoa = "62"
+url_index_vertebrata = "https://ftp.ensembl.org/pub/release-" + release_vertebrata + "/"
+url_index_metazoa = "https://ftp.ensemblgenomes.ebi.ac.uk/pub/release-" + release_metazoa + "/metazoa/"
+
+# REST request
 server = "https://rest.ensembl.org"
 ext = "/info/genomes/taxonomy/Primates?"
-version = "115"
-
+ext = "/info/genomes/taxonomy/Chordata?"
+ext = "/info/genomes/taxonomy/Metazoa?"
 r = requests.get(server+ext, headers={ "Content-Type" : "application/json"})
  
 if not r.ok:
@@ -27,22 +31,31 @@ for line in decoded:
     assembly_name = line["assembly_name"]
     assembly_accession = line["assembly_accession"]
     taxid = line["species_taxonomy_id"]
-    print(scientific_name + "\t" + str(taxid) + "\t" + assembly_name + "\t" + assembly_accession)
+    division = line["division"]
+    print(scientific_name + "\t" + str(taxid) + "\t" + assembly_name + "\t" + str(assembly_accession))
+    if division == "EnsemblVertebrates":
+        url_index = url_index_vertebrata
+        version = release_vertebrata
+    elif division == "EnsemblMetazoa":
+        url_index = url_index_metazoa
+        version = release_metazoa
+    else:
+        sys.exit("Wrong division")
 
-    url_fasta_genome = url_index + "release-" + version + "/" + "fasta/" + line["name"] + "/dna/" + line["url_name"] + "." +  line["assembly_default"] + ".dna.toplevel.fa.gz"
+    url_fasta_genome = url_index  + "fasta/" + line["name"] + "/dna/" + line["url_name"] + "." +  line["assembly_default"] + ".dna.toplevel.fa.gz"
     print(url_fasta_genome)
-    url_fasta_cds = url_index + "release-" + version + "/" + "fasta/" + line["name"] + "/cds/" + line["url_name"] + "." +  line["assembly_default"] + ".cds.all.fa.gz"
+    url_fasta_cds = url_index  + "fasta/" + line["name"] + "/cds/" + line["url_name"] + "." +  line["assembly_default"] + ".cds.all.fa.gz"
     print(url_fasta_cds)
-    url_fasta_pep = url_index + "release-" + version + "/" + "fasta/" + line["name"] + "/pep/" + line["url_name"] + "." +  line["assembly_default"] + ".pep.all.fa.gz"
+    url_fasta_pep = url_index + "fasta/" + line["name"] + "/pep/" + line["url_name"] + "." +  line["assembly_default"] + ".pep.all.fa.gz"
     print(url_fasta_pep)    
-    url_gff3 = url_index + "release-" + version + "/" + "gff3/" + line["name"] + "/" + line["url_name"] + "." +  line["assembly_default"] + "." + version + ".gff3.gz"
+    url_gff3 = url_index + "gff3/" + line["name"] + "/" + line["url_name"] + "." +  line["assembly_default"] + "." + version + ".gff3.gz"
     print(url_gff3)
-    dico[index] = [scientific_name,taxid,assembly_name,assembly_accession,url_fasta_cds,url_fasta_pep,url_fasta_genome,url_gff3] 
+    dico[index] = [scientific_name,taxid,assembly_name,assembly_accession,division,url_fasta_cds,url_fasta_pep,url_fasta_genome,url_gff3] 
     accessions.append(assembly_accession)
     index +=1
 
 sum = pd.DataFrame(dico).T
-sum.columns = ['Species Name','Taxid','Assembly Name','Assembly Accession',"URL fasta cds","URL fasta proteins","URL fasta genome","URL gff3 genome"]
+sum.columns = ['Species Name','Taxid','Assembly Name','Assembly Accession',"Division","URL fasta cds","URL fasta proteins","URL fasta genome","URL gff3 genome"]
 print(sum)
 sum.to_csv("organisms_data_ebi",sep="\t", index=False )
 
